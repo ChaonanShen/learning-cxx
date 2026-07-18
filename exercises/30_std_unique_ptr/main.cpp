b742035dc12a54d4b08d3af735d3b43f6edb634d
ChaonanShen <1686745041@qq.com>
2026-07-18 17:20:01 +0800

    总算写完了，有些题还真不简单

--- exercises/30_std_unique_ptr/main.cpp
+++ exercises/30_std_unique_ptr/main.cpp
@@ -42,10 +42,10 @@ int main(int argc, char **argv) {
     problems[0] = std::move(RECORDS);
 
     forward(drop(reset(forward(forward(reset(nullptr))))));
-    problems[1] = std::move(RECORDS);
+    problems[1] = std::move(RECORDS); // "ffr" "d" 最后一个forward收到null
 
     drop(drop(reset(drop(reset(reset(nullptr))))));
-    problems[2] = std::move(RECORDS);
+    problems[2] = std::move(RECORDS); // "r" "d" "d"
 
     // ---- 不要修改以上代码 ----
 
@@ -53,8 +53,8 @@ int main(int argc, char **argv) {
         {"fd"},
         // TODO: 分析 problems[1] 中资源的生命周期，将记录填入 `std::vector`
         // NOTICE: 此题结果依赖对象析构逻辑，平台相关，提交时以 CI 实际运行平台为准
-        {"", "", "", "", "", "", "", ""},
-        {"", "", "", "", "", "", "", ""},
+        {"d", "ffr"}, // 注意析构是反着来的，后生成的先析构
+        {"d", "d", "r"},
     };
 
     // ---- 不要修改以下代码 ----
